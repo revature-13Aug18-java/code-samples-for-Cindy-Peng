6f92e0e3bb93da920519ccbb4314f5b8a4292cc6
Cindy <cpeng3030@gmail.com>
2018-10-15 12:10:51 -0700

    removed some files I didn't work on

--- P2-BackEnd/src/main/java/com/revature/repositories/StockTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.models.StockTransaction;
-
-public interface StockTransactionRepository  extends JpaRepository<StockTransaction, Long>{
-	
-}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # code-samples-for-Cindy-Peng
+
+## P1 Reimbursements App
