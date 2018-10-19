cb3bc61261b5c4275a75ddc15cafbc6259c5f1cd
Cindy <cpeng3030@gmail.com>
2018-10-19 07:35:38 -0700

    filtering

--- P2-FrontEnd/src-app/components/login/login.component.ts
+++ P2-FrontEnd/src-app/components/login/login.component.ts
@@ -10,13 +10,6 @@ import { LocalStorageService } from '../../services/localstorage.service';
 })
 export class LoginComponent implements OnInit {
 
-  userId : number;
-  userN: string;
-  passW: string;
-  name: string;
-
-  showe : string;
-
   public user: User;
 
   constructor(private postService: CredentialsService, private localStorageService:LocalStorageService) { }
@@ -26,35 +19,5 @@ export class LoginComponent implements OnInit {
     //if session is open, and they go to /login, should just take them to home page
     if(this.localStorageService.getSaved("username") !== null)
       window.location.replace("home");
-  }
-
-  sendCredentials(){
-    this.user.userN = this.userN;
-    this.user.passW = this.passW;
-    this.postService.postCredentials(this.user).then(res =>
-      {
-      this.user.userId = res.userId;
-      console.log(this.user.userId);
-      this.user.userN = res.userN;
-      console.log(this.user.userN);
-      this.user.passW = res.passW;
-      console.log(this.user.passW);
-      this.user.name = res.name;
-      console.log(this.user.name);
-      if(this.user.name === undefined || this.user.userN === undefined
-        || this.user.userN === undefined || this.user.userId === undefined){
-          window.location.reload(true);
-          console.log("user not found");
-          this.user.userId = undefined;
-          this.user.name = undefined;
-        }else{
-          window.location.replace("home");
-          localStorage.setItem("userId", JSON.stringify(this.user.userId));
-          localStorage.setItem("username", this.user.userN);
-          localStorage.setItem("password", this.user.userN);
-          localStorage.setItem("name", this.user.userN);
-          window.location.replace("home");
-        }
-    });
   }
 }
--- P2-FrontEnd/src-app/services/credentials.service.ts
+++ P2-FrontEnd/src-app/services/credentials.service.ts
@@ -16,18 +16,6 @@ export class CredentialsService {
   loginUrl: string = "http://ccstockportfoliop2-env.hre7kq2up7.us-east-1.elasticbeanstalk.com/login";
   settingsUrl: string = "http://ccstockportfoliop2-env.hre7kq2up7.us-east-1.elasticbeanstalk.com/users";
 
-  //Send our entered username and password to the DB
-  postCredentials(usr: User): Promise<User> {
-  
-  const headers = new HttpHeaders({'Access-Control-Allow-Origin':'*'}).set('content-type','application/json');
-  var body = 
-  {
-    username: usr.userN,
-    password: usr.passW
-  };
-  return this.httpClient.post<User>(this.loginUrl, body, {headers} ).toPromise(); //rtns observable so needs toPromise()
-}
-
  //observable put function
  putFormData(formData:UserSettingsFormData) : Observable<User> {
   const headers = new HttpHeaders().set("content-type", "application/json");
@@ -41,21 +29,6 @@ export class CredentialsService {
   };
   
   return this.httpClient.put<User>(this.settingsUrl, body, {headers});
-}
-
-postUser(url: string, user: User) : Promise<User> {
-  const headers = new HttpHeaders().set("content-type", "application/json");
-  //must be named "headers" else problems during return
-  console.log("User before body: " + user);
-  let body = 
-  { 
-    userN: user.userN,
-    passW: user.passW,
-    name: user.name,
-  };
-  console.log("User body: " + body);
-  
-  return this.httpClient.post<User>(url, body, {headers}).toPromise();
 }
 
 }
